0fd4d3b935643ad2b3e1dff03f37c03d67466117
feffe1702 <fpm.asd@gmail.com>
2024-10-17 16:46:04 +0200

    [DONE] complete exercise

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)*/
 
+
 let emailListEl = document.getElementById("emailList");
 let buttonEl = document.getElementById("generateBtn");
 
 buttonEl.addEventListener("click" , function(){
+    emailListEl.innerHTML = ""
+    let emailGenerata;
     for (let i = 0; i < 10; i++) {
-        emailListEl.innerHTML += `<li class="col-5 d-flex justify-content-center">fpm.asd@gmail.com</li>`
-        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(resp => {
+            emailGenerata = resp.data.response;    
+            emailListEl.innerHTML += `<li class="col-5 d-flex justify-content-center">${emailGenerata}</li>`
+        })
+        .catch(error => {
+            console.error('Errore durante il recupero dell\'email:', error);
+        });
     }
 })
