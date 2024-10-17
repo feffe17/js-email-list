ba54b08aa75e0631bbe776865c9eca85af0f219c
feffe1702 <fpm.asd@gmail.com>
2024-10-17 16:06:56 +0200

    [WIP] renamed repo

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)*/
 
+let emailListEl = document.getElementById("emailList");
+let buttonEl = document.getElementById("generateBtn");
+
+buttonEl
