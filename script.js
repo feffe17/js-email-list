4c82961452d1db1286e94d77c486301b3c04311b
feffe1702 <fpm.asd@gmail.com>
2024-10-17 14:50:21 +0200

    [WIP] add mark up and bts-css

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/*Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+/*Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 
+indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)*/
